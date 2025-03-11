fff7075dbe61a9189f1d340e1e90c8babaea760a
767Bartek <barteklidzbarski9@gmail.com>
2025-03-11 14:08:56 +0100

    koncowo

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -25,63 +25,48 @@ namespace WpfApp3
         private string cyfry;
         private string haslo;
         Random losuj = new Random();
+
         private void generuj_btn_Click(object sender, RoutedEventArgs e)
         {
-            /* string haslo;
-             MaleiDuzeLitery = "abcdefgshijklmoprstuwxyzABCDEFGHIJKLMNOPRSTUWXYZ";
-             znakiSpecjalne = "!@#$%^&*";
-             int[] cyfry = new int[10];
-             int ile = 0;
-             ileznaow_txt.Text = ile.ToString();
-             if(malelitery_check.IsChecked == true)
-             {
-                 //Random rnd = new Random(ile);
 
-             }
-             if (cyfry_check.IsChecked == true)
-             {
-
-             }
-             if (specjalne_check.IsChecked == true)
-             {
-
-             }
-             Random rnd = new Random();
-            // for (int i = 0; i < Cyfry.Length; i++)
-
-             */
-            MaleiDuzeLitery = "abcdefgshijklmoprstuwxyzABCDEFGHIJKLMNOPRSTUWXYZ";
-            znakiSpecjalne = "!@#$%^&*";
-            cyfry = "1234567890";
-            if (malelitery_check.IsChecked == true) 
+            if (!int.TryParse(ileznaow_txt.Text, out int dlugoscHasla) || dlugoscHasla <= 0)
             {
-           //     if (!int.TryParse(ileznaow_txt.Text)
-               
-            /*    string wybrane = MaleiDuzeLitery;
-                if (malelitery_check.IsChecked != true)
-                {
-                    wybrane += malelitery_check;
-                }
-            */
-           // Random losuj = new Random();
-                losuj.Next(MaleiDuzeLitery.Length);
+                MessageBox.Show("Podaj poprawną liczbę znaków!");
+                return;
+            }
+
+            string dozwoloneZnaki = "";
+
+            if (malelitery_check.IsChecked == true)
+                dozwoloneZnaki += MaleiDuzeLitery;
+
+            if (cyfry_check.IsChecked == true)
+                dozwoloneZnaki += cyfry;
+
+            if (specjalne_check.IsChecked == true)
+                dozwoloneZnaki += znakiSpecjalne;
+
+           /* if (string.IsNullOrEmpty(dozwoloneZnaki))
+            {
+                MessageBox.Show("Wybierz co najmniej jeden typ znaków!");
+                return;
+            }
+ */
+
+            for (int i = 0; i < dlugoscHasla; i++)
+            {
+                int index = losuj.Next(dozwoloneZnaki.Length);
                 
             }
-            if (cyfry_check.IsChecked == true)
-            {
-                losuj.Next(cyfry.Length);
-            }
-            if(specjalne_check.IsChecked == true)
-            {
-                losuj.Next(znakiSpecjalne.Length);
-               // znakiSpecjalne == haslo.
-            }
 
+            MessageBox.Show($"{haslo}");
         }
 
         private void Zatwierdź_btn_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"Dane pracownika:{imie_txt.Text} {nazwisko_txt.Text} {Stanowisko_combo.SelectedItem.ToString()} Hasło:");
+           // Stanowisko_combo.SelectedItem.ToString();
+            MessageBox.Show($"Dane pracownika:Imię: {imie_txt.Text}Nazwisko: {nazwisko_txt.Text}Stanowisko: {Stanowisko_combo.SelectedItem.ToString()}");
         }
     }
+
 }
